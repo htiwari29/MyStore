ce5a3d605ef418401bd22ebe64918e3601b47d23
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-04 18:11:35 +0530

    created SnackBar

---
+++ app/src/main/java/com/mystore/activities/BaseActivity.kt
@@ -0,0 +1,32 @@
+package com.mystore.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.mystore.R
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+
+    fun showErrorSnackBar(message: String, errorMessage: Boolean) {
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_LONG)
+        val snackBarView = snackBar.view
+
+        if (errorMessage) {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarError))
+        } else {
+            snackBarView.setBackgroundColor(
+                ContextCompat.getColor(
+                    this@BaseActivity,
+                    R.color.colorSnackBarSuccess))
+        }
+        snackBar.show()
+    }
+}
--- app/src/main/java/com/mystore/activities/RegisterActivity.kt
+++ app/src/main/java/com/mystore/activities/RegisterActivity.kt
@@ -1,22 +1,32 @@
 package com.mystore.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatCheckBox
 import com.mystore.R
 
-class RegisterActivity : AppCompatActivity() {
+class RegisterActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         setupActionBar()
+
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+        btnRegister.setOnClickListener {
+            validateRegisterDetails()
+        }
+
         val tvLogin = findViewById<TextView>(R.id.tv_login)
         tvLogin.setOnClickListener {
             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
         }
     }
+
     private fun setupActionBar() {
         val toolbarRegisterActivity  = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_register_activity)
         setSupportActionBar(toolbarRegisterActivity)
@@ -28,5 +38,71 @@ class RegisterActivity : AppCompatActivity() {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_white_24dp)
         }
         toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
+    }
+
+    private fun validateRegisterDetails(): Boolean {
+        val etFirstName = findViewById<EditText>(R.id.et_first_name)
+        val etLastName = findViewById<EditText>(R.id.et_last_name)
+        val etEmail = findViewById<EditText>(R.id.et_email)
+        val etPassword = findViewById<EditText>(R.id.et_password)
+        val etConfirmPassword = findViewById<EditText>(R.id.et_confirm_password)
+        val cbTermsAndCondition = findViewById<AppCompatCheckBox>(R.id.cb_terms_and_condition)
+        return when {
+            TextUtils.isEmpty(etFirstName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_first_name),
+                    true)
+                false
+            }
+
+            TextUtils.isEmpty(etLastName.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_last_name),
+                    true)
+                false
+            }
+
+            TextUtils.isEmpty(etEmail.text.toString().trim { it <= ' ' }) ||
+                    !etEmail.text.toString().matches(
+                        Regex("[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"))-> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_email),
+                    true)
+                false
+            }
+
+            TextUtils.isEmpty(etPassword.text.toString().trim { it <= ' ' }) ||
+                    etPassword.text.toString().length < 6-> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_password),
+                    true)
+                false
+            }
+
+            TextUtils.isEmpty(etConfirmPassword.text.toString().trim { it <= ' ' }) -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_enter_confirm_password),
+                    true)
+                false
+            }
+
+            etPassword.text.toString().trim { it <= ' ' } != etConfirmPassword.text.toString()
+                .trim { it <= ' ' } -> {
+                showErrorSnackBar(
+                    resources.getString(R.string.err_msg_password_and_confirm_password_mismatch),
+                    true)
+                false
+            }
+            !cbTermsAndCondition.isChecked -> {
+                showErrorSnackBar(resources.getString(R.string.err_msg_agree_terms_and_condition),
+                    true)
+                false
+            }
+            else -> {
+                showErrorSnackBar("Your details are valid.",
+                    false)
+                true
+            }
+        }
     }
 }
