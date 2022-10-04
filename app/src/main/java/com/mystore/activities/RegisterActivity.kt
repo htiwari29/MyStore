354e5699713cf710259278ca84c6e31ba89a99c2
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-04 17:47:24 +0530

    added toolbar for convenience

--- app/src/main/java/com/mystore/activities/RegisterActivity.kt
+++ app/src/main/java/com/mystore/activities/RegisterActivity.kt
@@ -11,9 +11,22 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        setupActionBar()
         val tvLogin = findViewById<TextView>(R.id.tv_login)
         tvLogin.setOnClickListener {
             startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
         }
+    }
+    private fun setupActionBar() {
+        val toolbarRegisterActivity  = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_register_activity)
+        setSupportActionBar(toolbarRegisterActivity)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_white_24dp)
+        }
+        toolbarRegisterActivity.setNavigationOnClickListener { onBackPressed() }
     }
 }
