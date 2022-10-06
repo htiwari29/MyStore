3975ded1e5abc643881a3f4fb584431541276e6b
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-06 17:27:29 +0530

    created ForgotPasswordActivity

--- app/src/main/java/com/mystore/activities/ForgotPasswordActivity.kt
+++ app/src/main/java/com/mystore/activities/ForgotPasswordActivity.kt
@@ -1,12 +1,63 @@
 package com.mystore.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.mystore.R
 
-class ForgotPasswordActivity : AppCompatActivity() {
+class ForgotPasswordActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgot_password)
+        setupActionBar()
+
+        val btn_submit = findViewById<Button>(R.id.btn_submit)
+        btn_submit.setOnClickListener {
+
+            val et_email = findViewById<EditText>(R.id.et_email)
+            val email: String = et_email.text.toString().trim { it <= ' ' }
+
+            if (email.isEmpty()) {
+                showErrorSnackBar(resources.getString(R.string.err_msg_enter_email), true)
+            } else {
+
+                showProgressDialog(resources.getString(R.string.please_wait))
+
+                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
+                    .addOnCompleteListener { task ->
+
+                        hideProgressDialog()
+
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this@ForgotPasswordActivity,
+                                resources.getString(R.string.email_sent_success),
+                                Toast.LENGTH_LONG
+                            ).show()
+
+                            finish()
+                        } else {
+                            showErrorSnackBar(task.exception!!.message.toString(), true)
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setupActionBar() {
+
+        val toolbar_forgot_password_activity = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_forgot_password_activity)
+        setSupportActionBar(toolbar_forgot_password_activity)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_white_24dp)
+        }
+
+        toolbar_forgot_password_activity.setNavigationOnClickListener { onBackPressed() }
     }
 }
