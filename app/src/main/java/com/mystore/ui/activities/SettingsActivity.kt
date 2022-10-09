ec364343a7662dbc9b2cd012e022c0ed23f3045b
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-09 16:46:59 +0530

    implemented Edit profile button

--- app/src/main/java/com/mystore/ui/activities/SettingsActivity.kt
+++ app/src/main/java/com/mystore/ui/activities/SettingsActivity.kt
@@ -1,19 +1,29 @@
 package com.mystore.ui.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 import com.mystore.R
 import com.mystore.firestore.FirestoreClass
 import com.mystore.models.User
+import com.mystore.utils.Constants
 import com.mystore.utils.GlideLoader
 import kotlinx.android.synthetic.main.activity_settings.*
 
 @SuppressLint("SetTextI18n")
-class SettingsActivity : BaseActivity() {
+class SettingsActivity : BaseActivity(), View.OnClickListener {
+    private lateinit var mUserDetails: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         setupActionBar()
+
+        tv_edit.setOnClickListener(this@SettingsActivity)
+        btn_logout.setOnClickListener(this@SettingsActivity)
+        ll_address.setOnClickListener(this@SettingsActivity)
     }
 
     private fun setupActionBar() {
@@ -32,6 +42,7 @@ class SettingsActivity : BaseActivity() {
     }
 
     fun userDetailsSuccess(user: User) {
+        mUserDetails = user
         hideProgressDialog()
         GlideLoader(this@SettingsActivity).loadUserPicture(user.image, iv_user_photo)
         tv_name.text = "${user.firstName} ${user.lastName}"
@@ -43,6 +54,25 @@ class SettingsActivity : BaseActivity() {
     override fun onResume() {
         super.onResume()
         getUserDetails()
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.btn_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    val intent = Intent(this@SettingsActivity, LoginActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    finish()
+                }
+                R.id.tv_edit -> {
+                    val intent = Intent(this@SettingsActivity, UserProfileActivity::class.java)
+                    intent.putExtra(Constants.EXTRA_USER_DETAILS, mUserDetails)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
 }
