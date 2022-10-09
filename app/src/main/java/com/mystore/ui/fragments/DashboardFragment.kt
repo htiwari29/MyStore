fe4a474bfe0f928ec7c4688babf93594fe59369a
Himanshu Tiwari <htiwari292001@gmail.com>
2022-10-09 14:47:03 +0530

    created SettingsActivity

--- app/src/main/java/com/mystore/ui/fragments/DashboardFragment.kt
+++ app/src/main/java/com/mystore/ui/fragments/DashboardFragment.kt
@@ -1,18 +1,25 @@
 package com.mystore.ui.fragments
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.mystore.R
 import com.mystore.databinding.FragmentDashboardBinding
+import com.mystore.ui.activities.SettingsActivity
 
 @SuppressLint("SetTextI18n")
+@Suppress("DEPRECATION")
 class DashboardFragment : Fragment() {
     private var _binding: FragmentDashboardBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,14 +28,30 @@ class DashboardFragment : Fragment() {
     ): View {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
         val textView: TextView = binding.textDashboard
         textView.text = "This is dashboard Fragment"
         return root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    @Deprecated("Deprecated in Java")
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dashboard_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
+
+    @Deprecated("Deprecated in Java")
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(activity, SettingsActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+//    override fun onDestroyView() {
+//        super.onDestroyView()
+//        _binding = null
+//    }
 }
